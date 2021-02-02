b2725059041803172206ca6432a95be038c53d67
federicobg95 <federicobg95@gmail.com>
2021-02-02 17:47:21 -0500

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 //puerto
 //================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ===============================
+//Entorno
+//================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+
+//================================
+//Base de datos
+//================================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://federicobg95:WXZP7i9F6vP86Aeu@cluster0.sh5hr.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,47 +1,29 @@
+require('./config/config')
 const express = require('express')
+const mongoose = require('mongoose')
+
+
 const app = express();
+
+
 const bodyParser = require('body-parser');
 
-require('./config/config')
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
-});
+app.use(require('./routes/usuario'))
 
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id: id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delet Usuario');
-});
-
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(db => console.log('Base de datos en linea')).catch(err => console.error(err));
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando en el puerto 3000');
