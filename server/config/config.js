af50cf6465d31270508799010c33045919619a29
federicobg95 <federicobg95@gmail.com>
2021-02-03 10:00:05 -0500

    Commit para subir a heroku

--- server/config/config.js
+++ server/config/config.js
@@ -14,8 +14,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 //================================
 let urlDB;
 if (process.env.NODE_ENV === 'dev') {
+    //cuando este en local va a funcionar con esta url
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
+    //cuando este en produccion se va a conectar con la base de datos en mongoatlas
     urlDB = 'mongodb+srv://federicobg95:WXZP7i9F6vP86Aeu@cluster0.sh5hr.mongodb.net/cafe';
 }
 
